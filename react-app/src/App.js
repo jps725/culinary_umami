e078181765d860ebbaf5cbb336b19c199594cfa9
Josh Steinberg <jpsteinberg@gmail.com>
2021-06-03 17:47:59 -0400

    deleted excess route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import SignUpForm from "./components/auth/SignUpForm";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import { authenticate } from "./store/session";
 import SplashPage from "./components/SplashPage";
@@ -28,9 +27,6 @@ function App() {
       <Switch>
         <Route path="/splash" exact={true}>
           <SplashPage />
-        </Route>
-        <Route path="/signup" exact={true}>
-          <SignUpForm />
         </Route>
         <ProtectedRoute path="/profile" exact={true}>
           <Profile />
